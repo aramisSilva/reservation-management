da572fb63c49558de1c092eb1f914f4404cbca3f
aramis_ubuntu <aods@ic.ufal.br>
2024-03-19 14:37:27 -0300

    :wrench: chore(settings.py): Tuplas das configurações do rest_framework

--- management/settings.py
+++ management/settings.py
@@ -66,10 +66,13 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": "rest_framework.permissions.IsAuthenticated",
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ["Bearer"],
     "SIGNING_KEY": SECRET_KEY,
