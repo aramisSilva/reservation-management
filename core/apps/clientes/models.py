a5d8b107935f36d254325a6320507b2d7956461e
aramis_ubuntu <aods@ic.ufal.br>
2024-03-20 14:07:59 -0300

    :sparkles: feat(clientes): Feature do app de clientes

--- core/apps/clientes/models.py
+++ core/apps/clientes/models.py
@@ -1,5 +1,15 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from validate_docbr import CPF
+
+
+def validar_cpf(value):
+    cpf = CPF()
+    if not cpf.validate(value):
+        raise ValidationError(f'{value} não é um CPF válido.')
+
 
 class Cliente(AbstractUser):
-    pass
+    cpf = models.CharField('CPF', max_length=14, unique=True, validators=[validar_cpf],
+                           help_text='Informe o CPF no formato XXX.XXX.XXX-XX.')
